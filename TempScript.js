f7b832e4a453e6f0ab96d62578ba192d27ace3c1
ChillMan <jonathan.borgholm@gmail.com>
2024-04-28 01:39:26 +0200

    player list

--- TempScript.js
+++ TempScript.js
@@ -1,16 +1,42 @@
 const nameList = [];
-
-nameList.push("jonathan");
-nameList.push("Max");
-nameList.push("Oscar");
+const playerList = [];
 
 for (var i = 0; i < nameList.length; i++){
     console.log(nameList[i] + " " + i);
 }
 
-class player{
-    player(name, age){
+document.querySelector('#name').addEventListener('keydown', (e) => {
+    if(e.keyCode == 13){
+        const elem = e.target;
+        nameList.push(elem.value);
+        
+        document.querySelector('#name').value = '';
+
+        displayWords();
+    }
+});
+
+document.querySelector('#done').addEventListener('click', (e) => {
+    for(let i = 0; i < nameList.length; i++) {
+        const newPlayer = new player(nameList[i]);
+        playerList.push(newPlayer);
+        console.log("newPlayer: " + newPlayer.name);
+    }
+});
+
+function displayWords() {
+    const list = document.getElementById('wordList'); // Get the ul element
+    list.innerHTML = ''; // Empty the ul
+    nameList.forEach(word => {
+        const listItem = document.createElement('li'); // Create a new li element
+        listItem.textContent = word; // Set the text of li to the word
+        list.appendChild(listItem); // Append the li to the ul
+    });
+}
+
+class player {
+    constructor(name) {
         this.name = name;
-        this.age = age;
+        this.score = 0;
     }
 }
